f956e693ead56af6c765659093cc8254933316f1
LioTree <uc0110@163.com>
2023-03-31 22:38:46 +0800

    PA4: fix inheritsselftype

--- assignments/PA4/semant.cc
+++ assignments/PA4/semant.cc
@@ -104,14 +104,15 @@ ClassTable::ClassTable(Classes user_classes) : semant_errors(0) , error_stream(c
 
     for (auto it = classes.begin(); it != classes.end(); it++) {
         Symbol name = it->first;
-        Symbol parent = dynamic_cast<class__class *>(it->second)->get_parent();
-        if(parent != No_class && classes.find(parent) == classes.end()) {
-            semant_error(it->second) << "Class " << name << " inherits from an undefined class " << parent << "." << endl;
-        }
-        if(parent == Int || parent == Bool || parent == Str) {
+        Symbol parent = dynamic_cast<class__class *>(it->second)->get_parent(); 
+        if(parent == Int || parent == Bool || parent == Str || parent == SELF_TYPE) {
             semant_error(it->second) << "Class " << name << " cannot inherit class " << parent << "." << endl;
         }
-        inheritance_graph[name].push_back(parent);
+        else if(parent != No_class && classes.find(parent) == classes.end()) {
+            semant_error(it->second) << "Class " << name << " inherits from an undefined class " << parent << "." << endl;
+        }
+        else
+            inheritance_graph[name].push_back(parent);
     }
 }
 
